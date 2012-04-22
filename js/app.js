c6690a9ed7dc5eaef9111b5973a5d2764e24330f
Sam Johnson <sam@samwich.com>
2012-04-22 14:04:28 -0700

    select and delete

--- js/app.js
+++ js/app.js
@@ -1,11 +1,5 @@
 var App = Em.Application.create();
 
-App.MyView = Em.View.extend({
-  mouseDown: function() {
-    window.alert("hello world!");
-  },
-});
-
 App.initMap = function () {
   var myOptions = {
     center: new google.maps.LatLng(37.871667, -122.272778),
@@ -19,9 +13,15 @@ App.initMap = function () {
   });
 };
 
-App.markerView = Em.View.extend({
-	click: function (e) {
-		
+App.MarkerListItemView = Em.View.extend({
+	tagName: 'tr',
+	classNameBindings: ['isSelected'],
+	isSelected: function () {
+		return (this.get('content') === App.markers.get('selection'));
+	}.property('App.markers.selection'),
+	click: function () {
+		// console.log('click');
+		App.markers.set('selection', this.get('content'));
 	}
 });
 
@@ -29,11 +29,30 @@ App.markers = Em.ArrayProxy.create({
 	content: []
 });
 
+App.RemoveButtonView = Ember.Button.extend({
+	click: function () {
+		this.get('content').removeFromMap();
+		this.set('content', null);
+	}
+});
+
+App.MarkerDetailView = Ember.View.extend({
+	contentBinding: 'App.markers.selection'
+});
+
 App.Marker = Ember.Object.extend({
 	latLng: null,
-	lat: function () {return this.latLng.lat()}.property('latLng'),
-	lng: function () {return this.latLng.lng()}.property('latLng'),
-	bar: "bar"
+	latitude: function () {return this.latLng.lat()}.property('latLng'),
+	longitude: function () {return this.latLng.lng()}.property('latLng'),
+	removeFromMap: function () {
+		i = App.markers.get('content').indexOf(this);
+		App.markers.replaceContent(i, 1);
+	},
+	markerClick: function () {
+		console.log(self);
+		console.log("markerClick")
+		App.markers.set('selection', self);
+	}
 });
 
 App.mapController = Em.Object.create({
@@ -49,6 +68,10 @@ App.mapController = Em.Object.create({
 			var marker = new google.maps.Marker({
 				position: ll.get('latLng')
 			});
+			ll.marker = marker;
+			google.maps.event.addListener(marker, 'click', function() {
+				ll.markerClick();
+			});
 			that.markersDisplayed.pushObject(marker);
 			marker.setMap(App.map);
 		});
@@ -56,8 +79,7 @@ App.mapController = Em.Object.create({
 });
 
 App.ListView = Em.View.extend({
-	contentBinding: "App.markers.content",
-	foo: "foo"
+	contentBinding: "App.markers.content"
 });
 
 $().ready(function () {
