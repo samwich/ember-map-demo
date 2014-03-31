f06539149f38c120065919850568538978ce163d
Murtuza Boxwala <1mboxwala2@gmail.com>
2014-03-31 16:09:20 -0400

    Change MapView to extend CollectionView instead of ContainerView
    app.js:
    The general principle behind the design is the same, but instead of
    extending ContainerView, the MapView now extends CollectionView.
    CollectionView is more appropriate for displaying an array of data.  It
    automatically creates, destroys, and keeps track of its child view
    objects(in this case, MarkerView) as the underlying model changes.
    Specifically, this gets rid of the pushObject calls that were being
    made in the containerView when a new event was added.  Now, a
    MarkerView is automatically created and its didInsertElement function
    is called. We override the function to add a marker to the map.
    Also, removing markers is simplified.  You no longer need a boolean,
    'removed', on the EventController.  When the element is removed from
    the controller, the MarkerView's willDestroyElement function is called
    and we set the marker's map property to null, so it is removed from the
    map.
    By taking advantage of more powerful classes provided by Ember, I think
    the changes make the code a little simpler and more robust.
    
    index.html:
    1) created an events controller, instead of using the more ambiguous,
    Index controller
    2) made the map_canvas its own div instead of declaring it inside the
    ember view
    3) changed some names
    
    style.css:
    1) set the style of the map canvas as we are no longer doing this in
    the app.js file
    2) remove trailing spaces (my editor made me)

--- js/app.js
+++ js/app.js
@@ -1,135 +1,111 @@
 App = Ember.Application.create();
 
-App.Router.map(function() {
-  // put your routes here
+
+App.Event = Ember.Object.extend({
+  lat: null,
+  lng: null
 });
 
-App.Event = Ember.Object.extend({});
 
 App.EventController = Ember.ObjectController.extend({
   selected: false,
-  eltSelected: function(){
+
+  toggleSelected: function(){
     this.set("selected",!this.get("selected"));
   },
 });
 
-App.MarkerView = Ember.View.extend({
 
-  marker: null,
-
-  setup : function(mapEvent,eventCont,mapView){
-    console.log("setting up")
-    console.log(this.get('content'))
-    var marker = new google.maps.Marker({
-        position: mapEvent.latLng
-    });
-    this.set("marker",marker);
-    marker.setMap(mapView.get("map"));
-    this.set("controller", eventCont);
-
-    var that=this;
-    google.maps.event.addListener(marker, 'click', function() {
-        console.log("controller:")
-        that.get("controller").eltSelected();
-    });
-  },
-  markerIcon: function() {
-        if (this.get("controller").get("selected")) {
-          this.get("marker").setIcon("http://maps.google.com/mapfiles/ms/icons/green-dot.png");
-        } else {
-          this.get("marker").setIcon("http://maps.google.com/mapfiles/ms/icons/red-dot.png");
-        }
-  }.observes("controller.selected"),
-
-  removed: function(){
-    if(this.get("controller").get("model").get("removed")){
-      this.marker.setMap(null);
-      this.get("parentView").removeObject(this);
-    }
-  }.observes("controller.content.removed")
-
-})
-
-App.MapView = Ember.ContainerView.extend({
-
-  id: 'map_canvas',
-  tagName: 'div',
-
-  attributeBindings: ['style'],
-  style:"width:75%; height:300px",
-  
-  map:null,
-
-  didInsertElement: function() {
-    var mapOptions = {
-      center: new google.maps.LatLng(37.871667, -122.272778),
-      zoom: 13,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(this.$().get(0),mapOptions);
-    this.set("map",map);
-    
-    var that = this;
-    
-    // When clicking on the map, we create our own markerObject and pass it to our controller.
-    // The controller handles the marker management.
-    // The view handles placing the marker on the map.
-    google.maps.event.addListener(map, 'click', function(event){
-      eventCont= that.get("controller").addEvent(event.latLng.lat(), event.latLng.lng());
-      child = App.MarkerView.create();
-      child.setup(event, eventCont,that);
-      that.pushObject(child);
-    });
-  }
-});
-
-// This is our main array controller that does the marker management
-// It keeps a list of marker objects in its content
-App.IndexController = Ember.ArrayController.extend({
+App.EventsController = Ember.ArrayController.extend({
   content: [],
-  
-  // We add the marker to the ArrayControllers content.
-  addEvent: function (lat,lng) {
-    // create a new marker object containing the lat,lng and the googleMarker
-    var eventObject = App.Event.create({
-                            lat: lat,
-                            lng: lng
-    });
+  itemController: "event",
 
-    eventController = App.EventController.create();
-    eventController.set('model',eventObject);
-    this.content.pushObject(eventController);
-    return eventController;
+  addEvent: function (lat,lng) {
+    var eventObject = App.Event.create({lat: lat, lng: lng});
+    this.addObject(eventObject);
   },
 
-  // This property simply displays the number of selected markers.
-  selectedMarkerCounter: function () {
+  selectedCounter: function () {
     return this.filterProperty('selected', true).get('length');
   }.property('@each.selected'),
 
-
-  removeSelectedMarkers: function() {
+  removeSelected: function() {
     arr = this.filterProperty('selected', true);
-    if (arr.length==0) {
-        output = "nothing selected";
-    } else { 
-        for (var i = arr.length -1; i >= 0; i--) {
-          arr[i].get("model").set("removed",true);
-          this.content.removeObject(arr[i]);
-        }
-    }
+    this.removeObjects(arr);
   }
 });
 
-// we need to have an item-controller in place....
-// the selected property is stored on this controller (not on the object)
-// this controller simply observes the selected property to set the appropriate icon.
-App.MarkerController = Ember.ObjectController.extend({
-    markerIcon: function() {
-        if (this.get("selected")) {
-          this.content.googleMarker.setIcon("http://maps.google.com/mapfiles/ms/icons/green-dot.png");
-        } else {
-          this.content.googleMarker.setIcon("http://maps.google.com/mapfiles/ms/icons/red-dot.png");
-        }
-    }.observes("selected")
+
+App.MapView = Ember.CollectionView.extend({
+  mapTagId: "map_canvas",
+  map:null,
+
+  itemViewClass: 'App.MarkerView',
+  contentBinding: 'App.EventsController',
+
+  tagName: false, //all the html representation is handled by google maps
+
+  //lifecycle hooks
+  didInsertElement: function() {
+    var mapOptions = {
+      center: new google.maps.LatLng(40.440877, -79.958644),
+      zoom: 17,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(document.getElementById(this.mapTagId),
+                                   mapOptions);
+
+    var that = this;
+    google.maps.event.addListener(this.map, 'click', function(event){
+      that.clickMe(event.latLng.lat(), event.latLng.lng());
+    });
+  },
+
+  //ui
+  clickMe: function(lat, lng) {
+    this.get("controller").addEvent(lat, lng);
+  }
+});
+
+
+App.MarkerView = Ember.View.extend({
+  tagName: false, //all the html representation is handled by google maps
+  // marker: null,
+
+  iconMap: {
+    true: "http://maps.google.com/mapfiles/ms/icons/green-dot.png",
+    false: "http://maps.google.com/mapfiles/ms/icons/red-dot.png"
+  },
+  //lifecyclehooks
+  didInsertElement: function() {
+    //create google.maps.Marker
+    latLng = new google.maps.LatLng(this.content.get("lat"),
+                                    this.content.get("lng"));
+    markerOptions = {
+      position: latLng,
+      map: this.get("parentView").get("map"),
+      icon: this.iconMap[this.content.selected]
+    };
+    this.marker = new google.maps.Marker(markerOptions);
+
+    //set marker click listener
+    var that=this;
+    google.maps.event.addListener(this.marker, 'click', function() {
+        that.clickMe();
+    });
+  },
+
+  willDestroyElement: function() {
+    this.marker.setMap(null);
+  },
+
+  //ui
+  clickMe:function() {
+    this.content.toggleSelected();
+  },
+
+  //observers
+  setIcon: function() {
+    this.marker.setIcon(this.iconMap[this.content.selected]);
+  }.observes("content.selected")
 });
